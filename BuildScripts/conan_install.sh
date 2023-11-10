316f63033f5b24fb59a2f6d61b293276b13bd2bd
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2023-11-10 11:26:59 +0530

    changes for linux

---
+++ BuildScripts/conan_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# start from repo Build dir
+buildDir=$(dirname $0)
+cd $buildDir
+
+export CONAN_SYSREQUIRES_MODE=enabled
+export CONAN_REVISIONS_ENABLED=1
+export CONAN_NON_INTERACTIVE=1
+
+# install build tools
+# echo "Installing build tools..."
+# conan install conanfileBuildTools.txt --profile conanprofile.txt -if ../build/BuildTools || exit $?
+
+echo "Installing Conan dependencies..."
+
+if [[ $1 == "Debug" ]]
+then
+    conan install ../conanfile.py --profile conanprofile.txt -if ../build/Debug || exit $?
+else
+    conan install ../conanfile.py --profile conanprofile.txt -if ../build/Release || exit $?
+fi
