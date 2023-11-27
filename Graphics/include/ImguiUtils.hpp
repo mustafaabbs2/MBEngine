5eeea880ea9e24e3b2ae024e26c5a2b0df55a4ed
Mustafa Bhotvawala <mustafaabbs2@gmail.com>
2023-11-27 22:48:52 +0530

    CHANGES

--- Graphics/include/ImguiUtils.hpp
+++ Graphics/include/ImguiUtils.hpp
@@ -1,13 +1,10 @@
 
 #pragma once
 
-
-#include "imgui\imgui.h"
-#include "imgui\imgui_impl_glfw.h"
-#include "imgui\imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
-
 
 void setupImGUI(GLFWwindow* window);
