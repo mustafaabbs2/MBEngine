17b9c93448eca9842abf85a8e63d954975e06e01
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-04-23 11:06:53 +0530

    grpc test for very large array

--- RemotingService/client/src/headclient.cpp
+++ RemotingService/client/src/headclient.cpp
@@ -4,89 +4,143 @@
 
 #include <grpcpp/grpcpp.h>
 
-#include "kvp.grpc.pb.h"
+#include "headprotocol.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using kvstore::PutRequest;
-using kvstore::GetRequest;
-using kvstore::GetReply;
-using kvstore::PutReply;
-using kvstore::KVStore;
+//KVStore
+using HeadProtocol::GetReply;
+using HeadProtocol::GetRequest;
+using HeadProtocol::KVStore;
+using HeadProtocol::PutReply;
+using HeadProtocol::PutRequest;
 
-class KVStoreClient {
- public:
-  KVStoreClient(std::shared_ptr<Channel> channel)
-      : stub_(KVStore::NewStub(channel)) {}
+//Array transfer
+using HeadProtocol::ArrayTransfer;
+using HeadProtocol::LargeArray;
 
-  bool Put(const std::string& key, const std::string& value) {
-    PutRequest request;
-    //setting the two parts of the request struct
-    request.set_key(key);
-    request.set_value(value);
+class KVStoreClient
+{
+public:
+	KVStoreClient(std::shared_ptr<Channel> channel)
+		: stub_(KVStore::NewStub(channel))
+	{}
 
-    PutReply reply;
+	bool Put(const std::string& key, const std::string& value)
+	{
+		PutRequest request;
+		//setting the two parts of the request struct
+		request.set_key(key);
+		request.set_value(value);
 
-    ClientContext context;
-    Status status = stub_->Put(&context, request, &reply);
+		PutReply reply;
 
-    //not using reply anywhere..
+		ClientContext context;
+		Status status = stub_->Put(&context, request, &reply);
 
-    if (!status.ok()) {
-      std::cout << "Error putting key-value pair: " << status.error_message()
-                << std::endl;
-      return false;
-    }
-    return true;
-  }
+		//not using reply anywhere..
 
-  bool Get(const std::string& key, std::string* value) {
-    GetRequest request;
-    request.set_key(key);
+		if(!status.ok())
+		{
+			std::cout << "Error putting key-value pair: " << status.error_message() << std::endl;
+			return false;
+		}
+		return true;
+	}
 
-    GetReply reply;
-    ClientContext context;
-    Status status = stub_->Get(&context, request, &reply);
+	bool Get(const std::string& key, std::string* value)
+	{
+		GetRequest request;
+		request.set_key(key);
 
-    if (!status.ok()) {
-      std::cout << "Error getting key-value pair: " << status.error_message()
-                << std::endl;
-      return false;
-    }
-    *value = reply.value();
-    return true;
-  }
+		GetReply reply;
+		ClientContext context;
+		Status status = stub_->Get(&context, request, &reply);
 
- private:
-  std::unique_ptr<KVStore::Stub> stub_;
+		if(!status.ok())
+		{
+			std::cout << "Error getting key-value pair: " << status.error_message() << std::endl;
+			return false;
+		}
+		*value = reply.value();
+		return true;
+	}
+
+private:
+	std::unique_ptr<KVStore::Stub> stub_;
 };
 
+class ArrayTransferClient
+{
+public:
+	ArrayTransferClient(std::shared_ptr<Channel> channel)
+		: stub_(ArrayTransfer::NewStub(channel))
+	{}
 
-int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  KVStoreClient kvclient(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
+	int64_t SendArray(const std::vector<int>& array)
+	{
+		LargeArray request;
+		request.mutable_values()->CopyFrom({array.begin(), array.end()});
+		LargeArray response;
+		ClientContext context;
+		auto start = std::chrono::high_resolution_clock::now();
+		Status status = stub_->SendArray(&context, request, &response);
+		auto end = std::chrono::high_resolution_clock::now();
+		if(status.ok())
+		{
+			return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		}
+		else
+		{
+			std::cerr << "RPC failed: " << status.error_code() << ": " << status.error_message()
+					  << std::endl;
+			return -1;
+		}
+	}
 
-  std::string key("mustafa");
-  std::string value("99");
+private:
+	std::unique_ptr<ArrayTransfer::Stub> stub_;
+};
 
-  bool reply = kvclient.Put(key, value);
-  
-  if (reply)
-    std::cout << "KVStore set " << std::endl;
-  else
-    std::cout << "KVStore not set " << std::endl;
+int main(int argc, char** argv)
+{
+	// Instantiate the client. It requires a channel, out of which the actual RPCs
+	// are created. This channel models a connection to an endpoint (in this case,
+	// localhost at port 50051). We indicate that the channel isn't authenticated
+	// (use of InsecureChannelCredentials()).
 
-  std::string response;
+	std::shared_ptr<Channel> channel =
+		grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
 
-  // std::string wrongkey("44");
-  reply = kvclient.Get(key, &response);
-  
-  std::cout << "The value of the key: "<< key <<" is " << response << std::endl;
+	//Client 1
 
-  return 0;
+	KVStoreClient KVClient(channel);
+
+	std::string key("mustafa");
+	std::string value("99");
+
+	bool reply = KVClient.Put(key, value);
+
+	if(reply)
+		std::cout << "KVStore set " << std::endl;
+	else
+		std::cout << "KVStore not set " << std::endl;
+
+	std::string response;
+
+	// std::string wrongkey("44");
+	reply = KVClient.Get(key, &response);
+
+	std::cout << "The value of the key: " << key << " is " << response << std::endl;
+
+	//Client 2
+
+	ArrayTransferClient ArrayClient(channel);
+	std::vector<int> array(1000000, 1);
+	int64_t duration = ArrayClient.SendArray(array);
+	std::cout << "gRPC: Time taken to send the very large array: " << duration << " milliseconds"
+			  << std::endl;
+
+	return 0;
 }
--- RemotingService/server/src/headservice.cpp
+++ RemotingService/server/src/headservice.cpp
@@ -1,40 +1,68 @@
+#include "headprotocol.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
 #include <iostream>
 #include <unordered_map>
-#include <grpcpp/grpcpp.h>
-#include "kvp.grpc.pb.h"
 
-using namespace kvstore;
+using namespace HeadProtocol;
 using namespace std;
 
-class KVStoreImpl final : public KVStore::Service {
+//Class 1: Key Value Pair
+class KVStoreImpl final : public KVStore::Service
+{
 public:
-    grpc::Status Put(grpc::ServerContext* context, const PutRequest* request, PutReply* reply) override {
-        store_[request->key()] = request->value();
-        // A cout will not be displayed
-        // std::cout<<"Setting kvp on the server"<<std::endl;
-        reply->set_success(true);
-        return grpc::Status::OK;
-    }
+	grpc::Status
+	Put(grpc::ServerContext* context, const PutRequest* request, PutReply* reply) override
+	{
+		store_[request->key()] = request->value();
+		// A cout will not be displayed
+		// std::cout<<"Setting kvp on the server"<<std::endl;
+		reply->set_success(true);
+		return grpc::Status::OK;
+	}
 
-    grpc::Status Get(grpc::ServerContext* context, const GetRequest* request, GetReply* reply) override {
-        auto it = store_.find(request->key());
-        if (it == store_.end()) {
-            return grpc::Status(grpc::NOT_FOUND, "Key not found");
-        }
-        reply->set_value(it->second);
-        return grpc::Status::OK;
-    }
+	grpc::Status
+	Get(grpc::ServerContext* context, const GetRequest* request, GetReply* reply) override
+	{
+		auto it = store_.find(request->key());
+		if(it == store_.end())
+		{
+			return grpc::Status(grpc::NOT_FOUND, "Key not found");
+		}
+		reply->set_value(it->second);
+		return grpc::Status::OK;
+	}
 
 private:
-    unordered_map<string, string> store_;
+	unordered_map<string, string> store_;
 };
 
-int main() {
-    grpc::ServerBuilder builder;
-    KVStoreImpl service_;
-    builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
-    builder.RegisterService(&service_);
-    unique_ptr<grpc::Server> server(builder.BuildAndStart());
-    cout << "Server listening on 0.0.0.0:50051" << endl;
-    server->Wait();
+//Class 2: Transfer large arrays
+class ArrayTransferServiceImpl final : public ArrayTransfer::Service
+{
+public:
+	grpc::Status SendArray(grpc::ServerContext* context,
+						   const LargeArray* request,
+						   LargeArray* response) override
+	{
+		*response = *request;
+		return grpc::Status::OK;
+	}
+};
+
+int main()
+{
+
+	KVStoreImpl KVPService_;
+	ArrayTransferServiceImpl ArrayService_;
+
+	grpc::ServerBuilder builder;
+
+	builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+	builder.RegisterService(&KVPService_);
+	builder.RegisterService(&ArrayService_);
+
+	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+
+	cout << "Server listening on 0.0.0.0:50051" << endl;
+	server->Wait();
 }
