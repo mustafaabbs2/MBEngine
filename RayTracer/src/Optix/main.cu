c66ff19922d00925aef085c77a87cf63e2d70eb8
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-07-08 10:58:31 +0530

    fixes

--- RayTracer/src/Optix/main.cu
+++ RayTracer/src/Optix/main.cu
@@ -1,20 +1,39 @@
+#include <cuda_runtime.h>
 #include <iostream>
 #include <optix.h>
+#include <optix_function_table_definition.h>
+#include <optix_stubs.h>
 
 int main()
 {
-	// OptixDeviceContext context = nullptr;
-	// CUcontext cuCtx = 0; // zero means take the current context
-	// OptixDeviceContextOptions options = {};
-	// options.logCallbackFunction = nullptr;
-	// options.logCallbackLevel = 4;
+	// Initialize CUDA
+	CUcontext cuCtx = 0; // zero means take the current context
+	cudaFree(0); // Initialize CUDA
 
-	optixInit();
-	// optixDeviceContextCreate(cuCtx, &options, &context);
+	// Create an OptiX device context
+	OptixDeviceContext context = nullptr;
+	OptixDeviceContextOptions options = {};
+	options.logCallbackFunction = nullptr;
+	options.logCallbackLevel = 4;
+
+	CUresult cuRes = cuCtxGetCurrent(&cuCtx);
+	if(cuRes != CUDA_SUCCESS)
+	{
+		std::cerr << "Error getting current CUDA context" << std::endl;
+		return 1;
+	}
+
+	OptixResult optixRes = optixDeviceContextCreate(cuCtx, &options, &context);
+	if(optixRes != OPTIX_SUCCESS)
+	{
+		std::cerr << "Error creating OptiX device context" << std::endl;
+		return 1;
+	}
 
 	std::cout << "Hello, OptiX!" << std::endl;
 
-	// optixDeviceContextDestroy(context);
+	optixDeviceContextDestroy(context);
 
 	return 0;
 }
+
